e43eda4cee7209de7c109c46c191d510c04d85fc
Harry211201 <116766787+Harry211201@users.noreply.github.com>
2023-01-20 19:48:10 -0500

    login complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CategoriasComponent } from './expensable/categorias/categorias.component';
+import { PresupuestoComponent } from './expensable/presupuesto/presupuesto.component';
+import { PrincipalComponent } from './expensable/principal/principal.component';
+import { TransaccionesComponent } from './expensable/transacciones/transacciones.component';
+import { PermisionsGuard } from './guards/permisions.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'principal',
+    component: PrincipalComponent,
+    canActivate: [PermisionsGuard],
+    children: [
+      { path: 'categorias', component: CategoriasComponent },
+      { path: 'transactions', component: TransaccionesComponent },
+      { path: 'budgets', component: PresupuestoComponent },
+      { path: '**', component: CategoriasComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { AuthModule } from './auth/auth.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_ROUTES } from './app.routes';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { AuthService } from './services/auth.service';
+
+import { ToastrModule } from 'ngx-toastr';
+import { ExpensableModule } from './expensable/expensable.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AuthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    APP_ROUTES,
+    ToastrModule.forRoot(),
+    ExpensableModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthService,
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
